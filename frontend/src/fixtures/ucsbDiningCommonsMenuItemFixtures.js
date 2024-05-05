801d27b9f4f56bfab3152b4cd7641682f1f2dfd8
katelarrick <katelarrick@umail.ucsb.edu>
2024-05-05 16:01:44 -0700

    kl - added DiningCommonsMenuItem fixtures

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -0,0 +1,38 @@
+const ucsbDiningCommonsMenuItemFixtures = {
+    oneMenuItem:
+    [
+      {
+        "id": 1,
+        "name": "Baked Pesto Pasta with Chicken",
+        "diningCommonsCode": "Ortega",
+        "station": "Entree Specials"      
+      }
+    ],
+
+    threeMenuItems:
+    [
+        {
+            "id": 2,
+            "name": "Tofu Banh Mi Sandwich (v)",
+            "diningCommonsCode": "Ortega",
+            "station": "Entree Specials"         
+        },
+
+        {
+            "id": 3,
+            "name": "Chicken Caesar Salad",
+            "diningCommonsCode": "Ortega",
+            "station": "Entrees"   
+        },
+
+        {
+            "id": 4,
+            "name": "Cream of Broccoli Soup (v)",
+            "diningCommonsCode": "Portola",
+            "station": "Greens & Grains"        
+        },
+        
+    ]
+};
+
+export { ucsbDiningCommonsMenuItemFixtures };
