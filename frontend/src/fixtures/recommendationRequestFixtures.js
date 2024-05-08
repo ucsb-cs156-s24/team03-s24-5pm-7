0179afb3124fbda5b5f13c81c0bf75bc454688b2
adilahmed2 <102608569+adilahmed2@users.noreply.github.com>
2024-05-07 18:03:23 -0700

    added explanation

--- frontend/src/fixtures/recommendationRequestFixtures.js
+++ frontend/src/fixtures/recommendationRequestFixtures.js
@@ -3,6 +3,7 @@ const ucsbRecommendationRequestFixtures = {
         "id": 1,
         "requesterEmail": "adilahmed@ucsb.edu",
         "professorEmail": "pconrad@ucsb.edu",
+        "explanation": "please",
         "dateRequested": "2022-01-02T12:00:00",
         "dateNeeded": "2022-01-02T13:00:00",
         "done": false
@@ -12,6 +13,7 @@ const ucsbRecommendationRequestFixtures = {
             "id": 2,
             "requesterEmail": "adilahmed1@ucsb.edu",
             "professorEmail": "pconrad1@ucsb.edu",
+            "explanation": "please1",
             "dateRequested": "2022-01-02T14:00:00",
             "dateNeeded": "2022-01-02T15:00:00",
             "done": true
@@ -20,6 +22,7 @@ const ucsbRecommendationRequestFixtures = {
             "id": 3,
             "requesterEmail": "adilahmed2@ucsb.edu",
             "professorEmail": "pconrad2@ucsb.edu",
+            "explanation": "please2",
             "dateRequested": "2022-01-02T15:00:00",
             "dateNeeded": "2022-01-02T16:00:00",
             "done": true
@@ -28,6 +31,7 @@ const ucsbRecommendationRequestFixtures = {
             "id": 4,
             "requesterEmail": "adilahmed3@ucsb.edu",
             "professorEmail": "pconrad3@ucsb.edu",
+            "explanation": "please2",
             "dateRequested": "2022-01-02T16:00:00",
             "dateNeeded": "2022-01-02T17:00:00",
             "done": false
---
+++ frontend/src/main/components/RecommendationRequest/RecommendationRequestForm.js
@@ -0,0 +1,130 @@
+import { Button, Form, Row, Col } from 'react-bootstrap';
+import { useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
+
+function UCSBDateForm({ initialContents, submitAction, buttonLabel = "Create" }) {
+
+    // Stryker disable all
+    const {
+        register,
+        formState: { errors },
+        handleSubmit,
+    } = useForm(
+        { defaultValues: initialContents || {}, }
+    );
+    // Stryker restore all
+
+    const navigate = useNavigate();
+
+    // For explanation, see: https://stackoverflow.com/questions/3143070/javascript-regex-iso-datetime
+    // Note that even this complex regex may still need some tweaks
+
+    const isodate_regex = /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d)/i;
+    
+    // Stryker disable next-line Regex
+    const email_regex = /[a-zA-Z0-9]@[a-zA-Z0-9]+\.[a-zA-Z]/i;
+
+    return (
+
+        <Form onSubmit={handleSubmit(submitAction)}>
+
+
+            <Row>
+
+                {initialContents && (
+                    <Col>
+                        <Form.Group className="mb-3" >
+                            <Form.Label htmlFor="id">Id</Form.Label>
+                            <Form.Control
+                                data-testid="UCSBRecommendationRequestForm-id"
+                                id="id"
+                                type="text"
+                                {...register("id")}
+                                value={initialContents.id}
+                                disabled
+                            />
+                        </Form.Group>
+                    </Col>
+                )}
+
+                <Col>
+                    <Form.Group className="mb-3" >
+                        <Form.Label htmlFor="requesterEmail">RequesterEmail</Form.Label>
+                        <Form.Control
+                            data-testid="RecommendationRequestForm-requesterEmail"
+                            id="requesterEmail"
+                            type="text"
+                            isInvalid={Boolean(errors.requesterEmail)}
+                            {...register("requesterEmail", { required: true, pattern: email_regex})}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.requesterEmail && "Requester Email is required."}
+                            {errors.requesterEmail?.type === "pattern" && "Requester Email must be a correct email format."}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                </Col>
+                <Col>
+                    <Form.Group className="mb-3" >
+                        <Form.Label htmlFor="professorEmail">ProfessorEmail</Form.Label>
+                        <Form.Control
+                            data-testid="RecommendationRequestForm-professorEmail"
+                            id="professor"
+                            type="text"
+                            isInvalid={Boolean(errors.professorEmail)}
+                            {...register("professorEmail", { required: true, pattern: email_regex})}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.requesterEmail && "Professor Email is required."}
+                            {errors.requesterEmail?.type === "pattern" && "Professor Email must be a valid email."}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                </Col>
+            </Row>
+
+            <Row>
+
+                <Col>
+
+
+
+                    <Form.Group className="mb-3" >
+                        <Form.Label htmlFor="name">Name</Form.Label>
+                        <Form.Control
+                            data-testid="UCSBDateForm-name"
+                            id="name"
+                            type="text"
+                            isInvalid={Boolean(errors.name)}
+                            {...register("name", {
+                                required: "Name is required."
+                            })}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.name?.message}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                </Col>
+            </Row>
+
+            <Row>
+                <Col>
+                    <Button
+                        type="submit"
+                        data-testid="UCSBDateForm-submit"
+                    >
+                        {buttonLabel}
+                    </Button>
+                    <Button
+                        variant="Secondary"
+                        onClick={() => navigate(-1)}
+                        data-testid="UCSBDateForm-cancel"
+                    >
+                        Cancel
+                    </Button>
+                </Col>
+            </Row>
+        </Form>
+
+    )
+}
+
+export default UCSBDateForm;
