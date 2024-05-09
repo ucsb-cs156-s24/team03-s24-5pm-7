72cb3a8b01eb9f897c85b8728f6e2223a12406fc
Your Name <gracefeng@ucsb.edu>
2024-05-08 17:30:34 -0700

    Add Help Request fixtures

---
+++ frontend/src/fixtures/helpRequestFixtures.js
@@ -0,0 +1,43 @@
+const helpRequestFixtures = {
+    oneHelpRequest: {
+        "id": 1,
+        "requesterEmail": "gracefeng@ucsb.edu",
+        "teamID": "15",
+        "tableOrBreakoutRoom": "7",
+        "requestTime": "2022-01-02T12:00:00",
+        "explanation": "Dokku deployment issues.",
+        "solved": true
+    },
+    threeHelpRequests: [
+        {
+            "id": 1,
+            "requesterEmail": "gracefeng@ucsb.edu",
+            "teamID": "15",
+            "tableOrBreakoutRoom": "7",
+            "requestTime": "2022-01-02T12:00:00",
+            "explanation": "Dokku deployment issues.",
+            "solved": true
+        },
+        {
+            "id": 2,
+            "requesterEmail": "gracefeng@ucsb.edu",
+            "teamID": "16",
+            "tableOrBreakoutRoom": "8",
+            "localDateTime": "2022-04-03T12:00:00",
+            "explanation": "Dokku deployment issues.",
+            "solved": true
+        },
+        {
+            "id": 3,
+            "requesterEmail": "gracefeng@ucsb.edu",
+            "teamID": "17",
+            "tableOrBreakoutRoom": "9",
+            "localDateTime": "2022-07-04T12:00:00",
+            "explanation": "Dokku deployment issues.",
+            "solved": false
+        },
+    ]
+};
+
+
+export { helpRequestFixtures };
