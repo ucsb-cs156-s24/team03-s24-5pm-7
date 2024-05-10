d9304ed87a7801b9afe034b1c2d6a6053218c98f
Zhen Bi <zhenbi@csil.cs.ucsb.edu>
2024-05-10 00:23:54 -0700

    add create page for articles

--- frontend/src/main/pages/Articles/ArticlesCreatePage.js
+++ frontend/src/main/pages/Articles/ArticlesCreatePage.js
@@ -1,11 +1,51 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import ArticlesForm from "main/components/Articles/ArticlesForm";
+import { Navigate } from 'react-router-dom'
+import { useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
 
-export default function ArticlesCreatePage() {
+export default function ArticlesCreatePage({storybook=false}) {
+
+  const objectToAxiosParams = (articles) => ({
+    url: "/api/Articles/post",
+    method: "POST",
+    params: {
+      title: articles.title,
+      url: articles.url,
+      explanation: articles.explanation,
+      email: articles.email,
+      dateAdded: articles.dateAdded
+    }
+  });
+
+  const onSuccess = (articles) => {
+    toast(`New article Created - id: ${articles.id} title: ${articles.title}`);
+  }
+
+  const mutation = useBackendMutation(
+    objectToAxiosParams,
+     { onSuccess }, 
+     // Stryker disable next-line all : hard to set up test for caching
+     ["/api/Articles/all"]
+     );
+
+  const { isSuccess } = mutation
+
+  const onSubmit = async (articles) => {
+    mutation.mutate(articles);
+  }
+
+  if (isSuccess && !storybook) {
+    return <Navigate to="/articles" /> //Aricles
+  }
 
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Create page not yet implemented</h1>
+        <h1>Create New Article</h1>
+
+        <ArticlesForm submitAction={onSubmit} />
+
       </div>
     </BasicLayout>
   )
--- frontend/src/tests/pages/Articles/ArticlesCreatePage.test.js
+++ frontend/src/tests/pages/Articles/ArticlesCreatePage.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, waitFor, fireEvent, screen } from "@testing-library/react";
 import ArticlesCreatePage from "main/pages/Articles/ArticlesCreatePage";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
@@ -8,24 +8,67 @@ import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
 
+
+const mockToast = jest.fn();
+jest.mock('react-toastify', () => {
+    const originalModule = jest.requireActual('react-toastify');
+    return {
+        __esModule: true,
+        ...originalModule,
+        toast: (x) => mockToast(x)
+    };
+});
+
+const mockNavigate = jest.fn();
+jest.mock('react-router-dom', () => {
+    const originalModule = jest.requireActual('react-router-dom');
+    return {
+        __esModule: true,
+        ...originalModule,
+        Navigate: (x) => { mockNavigate(x); return null; }
+    };
+});
+
+
 describe("ArticlesCreatePage tests", () => {
 
-    const axiosMock = new AxiosMockAdapter(axios);
+    const axiosMock =new AxiosMockAdapter(axios);
 
-    const setupUserOnly = () => {
+    beforeEach(() => {
         axiosMock.reset();
         axiosMock.resetHistory();
         axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
         axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
-    };
+    });
 
-    const queryClient = new QueryClient();
-    test("Renders expected content", () => {
-        // arrange
 
-        setupUserOnly();
-       
-        // act
+    test("renders without crashing", () => {
+        const queryClient = new QueryClient();
+        render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <ArticlesCreatePage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+    });
+
+
+
+    test("when you fill in the form and hit submit, it makes a request to the backend", async () => {
+
+        const queryClient = new QueryClient();
+        const articles = {
+            id: 17,
+            title: "a new article",
+            url: "https://aNewArticle.com",
+            explanation: "This is a new article for testing create",
+            email: "create11@gmail.com",
+            dateAdded: "2022-07-17T00:00"
+        };
+
+        axiosMock.onPost("/api/Articles/post").reply( 202, articles );
+
         render(
             <QueryClientProvider client={queryClient}>
                 <MemoryRouter>
@@ -34,10 +77,45 @@ describe("ArticlesCreatePage tests", () => {
             </QueryClientProvider>
         );
 
-        // assert
-        expect(screen.getByText("Create page not yet implemented")).toBeInTheDocument();
+        await waitFor(() => {
+            expect(screen.getByTestId("ArticlesForm-title")).toBeInTheDocument();
+        });
+
+
+        const titleField = screen.getByTestId("ArticlesForm-title");
+        const urlField = screen.getByTestId("ArticlesForm-url");
+        const explanationField = screen.getByTestId("ArticlesForm-explanation");
+        const emailField = screen.getByTestId("ArticlesForm-email");
+        const dateAddedField = screen.getByTestId("ArticlesForm-dateAdded");
+        const submitButton = screen.getByTestId("ArticlesForm-submit");
+
+
+        fireEvent.change(titleField, { target: { value: 'a new article' } });
+        fireEvent.change(urlField, { target: { value: 'https://aNewArticle.com' } });
+        fireEvent.change(explanationField, { target: { value: 'This is a new article for testing create' } });
+        fireEvent.change(emailField, { target: { value: 'create11@gmail.com' } });
+        fireEvent.change(dateAddedField, { target: { value: '2022-07-17T00:00' } });
+
+        expect(submitButton).toBeInTheDocument();
+
+        fireEvent.click(submitButton);
+
+        await waitFor(() => expect(axiosMock.history.post.length).toBe(1));
+
+        expect(axiosMock.history.post[0].params).toEqual(
+            {
+
+            "title": "a new article",
+            "url": "https://aNewArticle.com",
+            "explanation": "This is a new article for testing create",
+            "email": "create11@gmail.com",
+            "dateAdded": "2022-07-17T00:00"
+        });
+
+
+        expect(mockToast).toBeCalledWith("New article Created - id: 17 title: a new article");
+        expect(mockNavigate).toBeCalledWith({ "to": "/articles" }); //articles
     });
 
 });
-
 
