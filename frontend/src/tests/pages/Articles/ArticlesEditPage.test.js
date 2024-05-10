b603ac018de33fbb21f428d6c726b09138dccb63
Zhen Bi <zhenbi@csil.cs.ucsb.edu>
2024-05-09 22:48:17 -0700

    delete unused import

--- frontend/src/tests/pages/Articles/ArticlesEditPage.test.js
+++ frontend/src/tests/pages/Articles/ArticlesEditPage.test.js
@@ -1,5 +1,4 @@
 import { render, screen } from "@testing-library/react";
-import PlaceholderEditPage from "main/pages/Articles/ArticlesEditPage";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
 
